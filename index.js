579bed51788b20f256bf4040ea88b916dba8ab83
khoa <ngochoa1702@gmail.com>
2021-03-13 19:27:55 +0700

    đa đc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine','ejs');
+app.set('view','./views');
+
+app.use('/public',express.static('./public'));
+
+
+app.listen(3000, () => {
+  console.log(`Server running/`);
+});
